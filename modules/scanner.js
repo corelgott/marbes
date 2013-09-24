ef26e93e6b3dda3fa64bb0a43f5cd5057a300070
Corelgott <mail@corelgott.de>
2013-09-24 23:44:12 +0100

    Extended functionality of the main scanner class to match new scan to pdf requirements

--- modules/scanner.js
+++ modules/scanner.js
@@ -1,54 +1,105 @@
-var exec     = require('child_process').exec;
+var exec = require('child_process').exec;
+var fs   = require('fs'); 
 
-module.exports =  {
-    options : function(config, resolution, mode) {
-        if (!resolution) 
-          resolution = 75;
+module.exports = {
+  scanner : function(configPath) {
+    var loadConfig = function() {
+      var config = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
-      	if (!mode)
-      	  mode = 'Color';
+      if (!config)
+        return;
+
+      if (!config.port) {
+      	console.log('No port in the configuration provided');
+      	return;
+      }
+      return config;
+    };
+    
+    var scanner = {
+        config : null,
+        /*
+         * sets the scanner device id to use for scan operations
+         */
+        setDeviceId : function(deviceId) {
+          if (!deviceId || typeof(deviceId) != 'string')
+            throw new Error('No or invalid deviceId: ' + deviceId);
+          
+          this.config.deviceId = deviceId;
+        },
       
-        return {
-            deviceId   : config.deviceId,
-            resolution : resolution,
-            mode       : mode
+        saveConfig : function(path) {
+          var cfgPath = path ? path : configPath;
+          fs.writeFileSync(cfgPath, JSON.stringify(this.config, null, 4), 'utf8');
+        },
+    
+        /*
+         * gets a list of attached devives
+         */
+        getDevices : function(callback) {
+          var command = 'scanimage -f \'{"deviceId" : "%d", "vendor" : "%v", "model" : "%m"}%n\'';
+      
+          exec(command, {encoding: 'binary', maxBuffer: 5000*1024}, function(error, stdout) {
+            var res = new Array();
+            // thi is properbly goona fail with multiple scanners attached
+            res.push(JSON.parse(stdout));
+            callback(res);
+          });
+        },
+        
+        /*
+         * helper method to parse the provided options
+         */
+        parseOptions : function(obj) {
+          if (!obj.resolution) 
+            obj.resolution = this.config.defaultResolution;
+
+        	if (!obj.mode)
+        	  obj.mode = this.config.defaultMode;
+
+          obj.deviceId = this.config.deviceId;
+          return obj;
+        },
+        
+        isConfigured : function() {
+          return typeof(this.config.deviceId) == "string";
+        },
+        
+        scanWithProgress : function(options, callback, done) {
+          var params = this.parseOptions(options);
+        
+          var command = 'scanimage\ --device\ ' + params.deviceId + '\ ' +
+        		            '--resolution\ ' + params.resolution + '\ ' +
+        		            '--mode\ ' + params.mode + '\  ';
+
+          var buffer = new Buffer('', 'binary');
+          console.log(command);
+        	// start to scan the image    
+        	exec(command, {encoding: 'binary', maxBuffer: 50000*1024}, function(error, stdout) {
+             		callback(stdout);
+          	}).on('close', function() {
+                done();
+        	});
+        },
+      
+        scanSync : function(options, callback, done) {
+          var params = this.parseOptions(options, this.config);
+        
+        	var command = 'scanimage\ --device\ ' + params.deviceId + '\ ' +
+        		            '--resolution\ ' + params.resolution + '\ ' +
+        		            '--mode\ ' + params.mode + '\ |\ pnmtojpeg';
+
+          var buffer = new Buffer('', 'binary');
+          console.log(command);
+        	// start to scan the image    
+        	exec(command, {encoding: 'binary', maxBuffer: 50000*1024}, function(error, stdout) {
+             		callback(stdout);
+          	}).on('close', function() {
+                done();
+        	});
         }
-    },
-    
-    getDevices : function(callback) {
-      exec(command, {encoding: 'binary', maxBuffer: 5000*1024}, function(error, stdout) {
-        console.log(stdout)
-        callback(stdout);
-      });
-    }
-    
-    scanWithProgress : function() {
-      var command = 'scanimage\ --device\ ' + options.deviceId + '\ ' +
-    		            '--resolution\ ' + options.resolution + '\ ' +
-    		            '--mode\ ' + options.mode + '\  ';
-
-      var buffer = new Buffer('', 'binary');
-      console.log(command);
-    	// start to scan the image    
-    	exec(command, {encoding: 'binary', maxBuffer: 50000*1024}, function(error, stdout) {
-         		callback(stdout);
-      	}).on('close', function() {
-            done();
-    	});
-    },
-    
-    scanSync : function(options, callback, done){
-    	var command = 'scanimage\ --device\ ' + options.deviceId + '\ ' +
-    		            '--resolution\ ' + options.resolution + '\ ' +
-    		            '--mode\ ' + options.mode + '\ |\ pnmtojpeg';
-
-      var buffer = new Buffer('', 'binary');
-      console.log(command);
-    	// start to scan the image    
-    	exec(command, {encoding: 'binary', maxBuffer: 50000*1024}, function(error, stdout) {
-         		callback(stdout);
-      	}).on('close', function() {
-            done();
-    	});
-    }
-}
+      }
+      scanner.config = loadConfig();
+      return scanner;
+  }
+};

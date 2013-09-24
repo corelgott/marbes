d756dbb0de53369a161d554fcbd60ef081f9b7e5
Corelgott <mail@corelgott.de>
2013-09-24 23:42:44 +0100

    Extended main angular controller to used the new features provided by marbles.js

--- files/controllers/main.js
+++ files/controllers/main.js
@@ -1,15 +1,44 @@
 var app = angular.module('app',[]);
 
-app.controller('main', ['$scope', function($scope) {
-    $('#indicator').activity({color : '#ffffff'});
-  
-    $scope.resolutions = [75, 150, 300, 600];
-    $scope.resolution  = $scope.resolutions[0];
-    
-    $scope.modes       = ['Color', 'Gray', 'Black'];
-    $scope.mode        = $scope.modes[0];
-    
-    $scope.scanning    = false;
+app.controller('main', ['$scope', '$http', function($scope, $http) {
+    // the init wrapped up
+    $scope.init = function() {
+      // create the litte busy-indicator
+      $('#indicator').activity({color : '#ffffff'});
+
+      // we are not scanning right now
+      $scope.scanning    = false;
+      // flag if all necessary data has been loaded or not
+      $scope.ready       = false;
+      
+      // get the available options from the scanner
+      $http({method: 'GET', url: './getOptions'}).success(function(options) {
+        console.log('got response!');
+        console.log(options);
+      
+        $scope.resolutions = options.resolutions;
+        $scope.resolution  = options.defaultResolution;
+
+        $scope.modes       = options.modes;
+        $scope.mode        = options.defaultMode;
+      });
+      
+      $http({method:'GET', url : './isConfigured'}).success(function(status) {
+        if (status.isConfigured) {
+          $scope.ready = true;
+        } else
+          $http({method:'GET', url : './getConfiguration'}).success(function(config) {
+            if (config.status == 'auto') {
+              $scope.ready = true;
+            } else {
+              console.log(config.devs);
+              //show the user some option to choose from the devs
+            }
+          });
+      });
+    };
+    // call init
+    $scope.init();
     
     $scope.scan = function() {
       // new img
@@ -46,4 +75,27 @@ app.controller('main', ['$scope', function($scope) {
       // start scan
       $('#image').attr('src', './preview?date=' + new Date().getTime());
     }
+    
+    $scope.scanToPdf = function() {
+      $scope.scanning = true;
+      var _inner = function() {
+        $http( { method :'GET', 
+                 url    : './scanToPdf', 
+                 params : { anotherPage : true,
+                            resolution  : $scope.resolution, 
+                            mode        : $scope.mode }
+               }).success(function(resp) {
+          if (resp.status == 'proceed') {
+            if (confirm('Do you want to scan another page?'))
+              _inner();
+            else {
+              $.fileDownload('./scanToPdf?another=false');
+              $scope.scanning = false;
+            }
+          }
+        });
+      };
+      _inner();
+    }
+    
 }]);
